f90060fee9bdf68f9e7c9a714d6afae5f0bcc34a
Rian Apriansyah <jmryan73@gmail.com>
2020-01-04 23:54:59 +0700

    update get name

--- process.py
+++ process.py
@@ -6,6 +6,7 @@
 import argparse
 import os
 import re
+import pycountry
 from bs4 import BeautifulSoup
 from nltk.tag import StanfordNERTagger
 from nltk.tokenize import word_tokenize
@@ -14,6 +15,10 @@ st = StanfordNERTagger('/Users/rianapriansyah/Downloads/stanford-ner-2018-10-16/
 
 def html_parser(file_path):
 	# Recognize a file at filePath and save the extracted text to result.xml before extract intended information fromm it.
+	customer_name = ""
+	location = ""
+	customer_address = ""
+
 	print("Uploading..")
 	
 	with open(file_path, "r") as f:
@@ -21,26 +26,27 @@ def html_parser(file_path):
 
 	soup = BeautifulSoup(contents, 'html.parser')
 
-	customer_name = ""
+	alltext = ""
+
+	for txt in soup.find_all('p'):
+		alltext += txt.text
+		alltext += " "
+
+	splitted_text = alltext.split()
+	tag = st.tag(splitted_text)
+	locations = [(i, k) for (i, k) in tag if k == 'LOCATION']
+	people = [(i, k) for (i, k) in tag if k == 'PERSON']
+
+	customer_name = get_customer_name(people, splitted_text)
+	location = get_location(locations)
+	customer_address = get_customer_location(location, alltext)
+
 
 	for texts in soup.find_all('p'):
 		text = texts.text
-		if text is not "":
-			text = clean_text(text)
-			sp = text.split(sep=" ")
-			if len(sp) > 4:
-				continue
-			else:
-				tagged = st.tag(sp)
-				if tagged[0][1] == 'PERSON':
-					customer_name = text
-					break
-
-	for texts in soup.find_all('p'):
-		text = texts.text
-		if text is not "":
+		if text is  "":
 			#text = clean_text(text)
-			x = re.search(r"\d{1,3}.?\d{0,3}\s[a-zA-Z]{2,30}\s[a-zA-Z]{2,15}", text)
+			x = re.search(r"(\d{2,3}.?)\s([a-zA-Z ]{2,30})", text)
 			if x is not None:
 				customer_address = x.string
 				break
@@ -51,11 +57,24 @@ def html_parser(file_path):
 
 	print()
 
+def get_location(locations):
+	singapore_address = re.compile(r"(\d{2,3}.?)\s([a-zA-Z ]{2,30})")
+
+	for loc in locations:
+		country = pycountry.countries.get(name=loc[0])
+		if country is not None:
+			return country.name
+			
+def get_customer_location(location, text):
+	
+	return ""
+
 def clean_text(text):
-	alphanum = re.compile(r"[^a-zA-Z ]")
+	alphanum = re.compile(r"[^a-zA-Z0-9][\W]")
 	ws = re.compile(r"[\s]+")
-	text = re.sub(alphanum, '#', text)
+	text = re.sub(alphanum, ' ', text)
 	text = re.sub(ws, ' ', text)
+
 
 	return text
 
@@ -80,18 +99,31 @@ def process_text_file():
 	
 	print("Result was written to {}".format("Extracted.txt"))
 
-def get_customer_name():
+def get_customer_name(ppl, txts):
 	#get customer name from extracted text. 
-	file = open("Output.txt", "r")
-	name = ""
-	for line in file:
-		tagged = st.tag(word_tokenize(line))
-		if tagged[0][1] == 'PERSON':
-			name = line
-			break
-
-	file.close()
-	return name
+	customer_name = ""
+	first_name = ""
+	last_name = ""
+	for i, v in enumerate(ppl):
+		if i is not 0:
+			if len(customer_name.split()) is 1:
+				x = txts.index(customer_name) + 1
+				if txts.index(v[0]) is x:
+					last_name += v[0]
+					customer_name += " " + last_name
+				else:
+					break
+			else:
+				x = txts.index(last_name) + 1
+				if txts.index(v[0]) is x:
+					last_name = v[0]
+					customer_name += " " + last_name
+				else:
+					break
+		else:
+			first_name += v[0]
+			customer_name = first_name
+	return customer_name
 
 def get_month():
 	#function to get date of statement. Assuming the date in a short name format, (eg: Jan, Feb)
