91fa9f06e9fbf1c46c23ca0d7009ea754e718f52
Rian Apriansyah <jmryan73@gmail.com>
2020-01-06 13:44:37 +0700

    checkpoint

--- process.py
+++ process.py
@@ -15,16 +15,18 @@ st = StanfordNERTagger('/Users/rianapriansyah/Downloads/stanford-ner-2018-10-16/
 
 def html_parser(file_path):
 	# Recognize a file at filePath and save the extracted text to result.xml before extract intended information fromm it.
-	customer_name = ""
 	location = ""
+	customer_name = ""
 	customer_address = ""
+	customer_acc_number = ""
+	document_date = ""
 
 	print("Uploading..")
 	
 	with open(file_path, "r") as f:
 		contents = f.read()
 
-	soup = BeautifulSoup(contents, 'html.parser')
+	soup = BeautifulSoup(contents, 'lxml')
 
 	alltext = ""
 
@@ -33,71 +35,86 @@ def html_parser(file_path):
 		alltext += os.linesep
 
 	splitted_text = alltext.split()
-	tag = st.tag(splitted_text)
+	tag = st.tag(splitted_text) 
 	locations = [(i, k) for (i, k) in tag if k == 'LOCATION']
 	people = [(i, k) for (i, k) in tag if k == 'PERSON']
 
 	customer_name = get_customer_name(people, splitted_text)
 	location = get_location(locations)
-	customer_address = get_customer_location(location, alltext)
+	customer_address = get_customer_location(location, soup, customer_name)
+	document_date = get_month(splitted_text)
+	customer_acc_number = get_acc_number(alltext)
 
-	for texts in soup.find_all('p'):
-		text = texts.text
-		if text is  "":
-			#text = clean_text(text)
-			x = re.search(r"(\d{2,3}.?)\s([a-zA-Z ]{2,30})", text)
-			if x is not None:
-				customer_address = x.string
-				break
-			else:
-				continue
-			
+	result = "Name of Customer : " + customer_name + os.linesep + "Address of Customer : " + customer_address + os.linesep + "Bank Account number : " + customer_acc_number + os.linesep + "Statement Date : " + document_date 
+
+	with open("Result.txt", "w") as text_file:
+    			text_file.write(result)
+
+	print("Result was written to {}".format("Result.txt"))
+
+def get_acc_number(txt):
+	acc_number = ""
+	acc_number_pattern = re.compile(r"^[0-9-]{7,10}")
+	for i in iter(txt.splitlines()):
+		result = re.match(acc_number_pattern, i)
+		if result is not None:
+			acc_number = result.string
+			return acc_number
 
 
-	print()
 
 def get_location(locations):
-	singapore_address = re.compile(r"(	.?)\s([a-zA-Z ]{2,30})")
-
+	#get location of issued document to decide the regex pattern for address and postal code
 	for loc in locations:
 		country = pycountry.countries.get(name=loc[0])
 		if country is not None:
 			return country.name
 			
-def get_customer_location(loc, text):
-	address = ""
-	address_pattern = re.compile(r"\d{2,3}.?(?:[A-Za-z0-9.-]+[ ]?)+(?:Avenue|Lane|Road|Boulevard|Drive|Street|Ave|Dr|Rd|Blvd|Ln|St)\.?")
-	postal_code_pattern = ""
+def get_customer_location(loc, bsoup, cust_name):
+	#get customer address from document using regex pattern. The pattern needs to be determined based on the issued document location
+
+	#need to get reference of list of official postal code
 	if loc == 'Singapore':
-		postal_code_pattern = re.compile(r"Singapore \d{6}")
+		postal_code_pattern = re.compile(r"\d{6}")
+
+	text = ""
+	after_name_block = False
+	for txt in bsoup.find_all('p'):
+		if txt.text == cust_name:
+			after_name_block = True
+		
+		if after_name_block is True:
+			text += txt.text
+			text += os.linesep
+
+	address = ""
+	postal_code = "" 
+	address_pattern = re.compile(r"\d{2,3}.?(?:[A-Za-z0-9.-]+[ ]?)+(?:Avenue|Lane|Road|Boulevard|Drive|Street|Ave|Dr|Rd|Blvd|Ln|St)\.?", flags=re.IGNORECASE)
 	
 	for line in iter(text.splitlines()):
 		
-		result = re.match(address_pattern, line)
-		if result is not None:
-			address = result.string
-			return address
+		if address is "":
+			result = re.match(address_pattern, line)
+			if result is not None:
+				address = result.string
+		
+		if postal_code is "":
+			res = re.search(postal_code_pattern, line)
+			if res is not None:
+				postal_code = res.string
 
+		if address is not "" and postal_code is not "":
+			break
+
+	address += ", " + postal_code
+
+	return address
 
 def create_parser():
 	parser = argparse.ArgumentParser(description="Recognize a file via web service")
 	parser.add_argument('source_file')
 
 	return parser
-
-def process_text_file():
-	#function to process extracted certain information from OCR output
-
-	extracted = ""
-
-	extracted += "Name of customer : " + get_customer_name()
-	extracted += "Statement Date : " + get_month()
-
-	with open("Extracted.txt", "w") as text_file:
-    			text_file.write(extracted)
-
-	
-	print("Result was written to {}".format("Extracted.txt"))
 
 def get_customer_name(ppl, txts):
 	#get customer name from extracted text. 
@@ -127,13 +144,10 @@ def get_customer_name(ppl, txts):
 			customer_name = first_name
 	return customer_name
 
-def get_month():
+def get_month(txt):
 	#function to get date of statement. Assuming the date in a short name format, (eg: Jan, Feb)
 	#tokenize extracted text and then iterate each word to match dict key to get correct name of month.
 	#Get previous word of recognize name of month and get next word, still need a lot of improvements.
-	file = open("Output.txt", "r")
-	text_content = file.read()
-	file.close()
 
 	stat_date_is_found = False
 	date = ""
@@ -153,31 +167,28 @@ def get_month():
 		'Dec': 'December'
 	}
 
-	words = word_tokenize(text_content)
-
-	for index, word in enumerate(words):
+	for index, word in enumerate(txt):
 		if not stat_date_is_found:
-			month = months.get(words[index])
+			month = months.get(txt[index])
 			if month is not None:
-				date = words[index - 1] + " " + month + " " + words[index + 1]
+				date = txt[index - 1] + " " + month + " " + txt[index + 1]
 				stat_date_is_found = True
-		
+		else:
+			break
 
 	return date
 
 def main():
-	#args = create_parser().parse_args()
+	args = create_parser().parse_args()
 
-	#source_file = args.source_file
+	source_file = args.source_file
 
 	#activate this line for debugger purpose
-	#source_file = "Output.html" 
-	source_file = "bankstatement.html" 
+	#source_file = "bankstatement.html" 
 
 
 	if os.path.isfile(source_file):
 		html_parser(source_file)
-		#process_text_file()
 	else:
 		print("No such file: {}".format(source_file))
 
